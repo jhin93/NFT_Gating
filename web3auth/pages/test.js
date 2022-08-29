c9b05eec1566cd731fb758968fca2a4dafca42bd
jhin93 <scene1993@gmail.com>
2022-08-29 20:43:55 +0900

    일단 test.js에 콘솔로 데이터 찍는 것 까지 성공

---
+++ web3auth/pages/test.js
@@ -0,0 +1,22 @@
+
+import Moralis from 'moralis';
+import { EvmChain } from '../node_modules/@moralisweb3/evm-utils';
+
+const address = '0x60255c6De2cf768D5cE89f54eDb42D4E374a3177';
+
+const chain = EvmChain.MUMBAI;
+
+export default function test() {
+    Moralis.start({
+        apiKey: 'wuTRXSGYCbL42JbmFQH7SKkKCg6OE6uDuWJS3moULHsKK912VMGJanJvEE7JRFvf',
+        // ...and any other configuration
+    });
+    
+    const response = Moralis.EvmApi.account.getNFTs({
+        address,
+        chain,
+    });
+    console.log("response : ", response);
+}
+
+
