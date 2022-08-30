7f93a8c32e8675a44283bcbd6df4cc060252790e
jhin93 <scene1993@gmail.com>
2022-08-30 11:46:23 +0900

    중간커밋

--- web3auth/pages/index.js
+++ web3auth/pages/index.js
@@ -1,5 +1,41 @@
-function HomePage() {
-    return <div>Welcome to Next.js! Go to http://localhost:3000/signin</div>;
+
+import Moralis from 'moralis';
+import { EvmChain } from '../node_modules/@moralisweb3/evm-utils';
+
+const address = '0x60255c6De2cf768D5cE89f54eDb42D4E374a3177';
+const chain = EvmChain.MUMBAI;
+let response;
+
+function test() {
+    Moralis.start({
+        apiKey: 'wuTRXSGYCbL42JbmFQH7SKkKCg6OE6uDuWJS3moULHsKK912VMGJanJvEE7JRFvf',
+    });
+    
+    response = Moralis.EvmApi.account.getNFTs({
+        address,
+        chain,
+    });
+    // response.then((res) => {console.log("what response is : ", res._data.result)})
+
+    return {
+        props: {
+            nfts: response.then((res) => {console.log("what response is : ", res._data.result)})
+        }
+    }
 }
+
+test()
+
+
+
+function HomePage() {
+    return (
+        <div>
+            Welcome to Next.js! Go to http://localhost:3000/signin
+        </div>
+    )
+}
+
+
 
 export default HomePage;
