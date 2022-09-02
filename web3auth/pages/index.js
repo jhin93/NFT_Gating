ff30bb32f32dbf2d7537efe831738213386abf1c
jhin93 <scene1993@gmail.com>
2022-09-02 10:51:29 +0900

    겨우 됐다

--- web3auth/pages/index.js
+++ web3auth/pages/index.js
@@ -2,24 +2,12 @@
 import Moralis from 'moralis';
 import { EvmChain } from '@moralisweb3/evm-utils';
 
-const address = '0x60255c6De2cf768D5cE89f54eDb42D4E374a3177';
+const address = process.env.ACCOUNT;
 const chain = EvmChain.MUMBAI;
-let response;
+let testnft;
 
 
-function HomePage(nftData) {
-    // Moralis.start({
-    //     apiKey: 'wuTRXSGYCbL42JbmFQH7SKkKCg6OE6uDuWJS3moULHsKK912VMGJanJvEE7JRFvf',
-    // });
-    
-    // response = Moralis.EvmApi.account.getNFTs({
-    //     address,
-    //     chain,
-    // });
-    // response.then((res) => {
-    //     console.log("nfts : ", res._data.result)
-    // })
-    
+function HomePage({nftData}) {
     console.log("nftData : ", nftData)
 
     return (
@@ -37,13 +25,11 @@ export async function getServerSideProps() {
     const nftData = await Moralis.EvmApi.account.getNFTs({
         address,
         chain 
-    }).then((res) => {res._data});
-
-
+    })
 
     return {
         props: {
-            nftData: nftData
+            nftData: JSON.parse(JSON.stringify(nftData))
         }
     }
 }
